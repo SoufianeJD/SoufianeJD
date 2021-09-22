c29aba33656247bd12bce57f3aaed8e7c6fd00e0
SoufianeJD <s.f.j.dsapro@gmail.com>
2021-09-22 07:45:57 -0700

    test3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter , Route, Switch } from "react-router-dom";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -24,7 +24,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <HashRouter>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -36,7 +36,7 @@ function App() {
           <Route path="/resume" component={Resume} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
